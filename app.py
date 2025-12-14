56587dcd2c4b04a1c34ea674951896e350676843
Piyush <testawsaws3@gmail.com>
2025-12-14 20:47:04 +0000

    work

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = FastAPI()
 # API endpoint that returns todos from JSONPlaceholder API
 @app.get("/")
 async def hello():
-    return {"message": "Hello World from Python ECS! with V3"}
+    return {"message": "Hello World from Python ECS! with V4"}
 
 @app.get("/todos")
 async def get_todos():
